c76ddc5d8a7163cf98ab0c12e467630da46cdb1b
ACKAPOB <ACKAPOB@yandex.ru>
2023-01-12 11:58:41 +0300

    Commit Ver.6.7. 124~~

--- ewm-service/src/main/java/ru/practicum/ewm/category/repository/CategoryRepository.java
+++ ewm-service/src/main/java/ru/practicum/ewm/category/repository/CategoryRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-
+    //vb/,. .,mcv b,.v
     @Query(value = "SELECT * FROM categories c " +
             "LEFT JOIN events e on c.id = e.category_id " +
             "WHERE category_id=:id", nativeQuery = true)
