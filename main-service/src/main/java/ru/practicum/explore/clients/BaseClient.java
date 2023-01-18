a56329fcf509e3074e36a8babab8e1f0f59afa7c
ACKAPOB <ACKAPOB@yandex.ru>
2023-01-18 12:40:46 +0300

    Commit 18/01/23 Afte Check

--- main-service/src/main/java/ru/practicum/explore/clients/BaseClient.java
+++ main-service/src/main/java/ru/practicum/explore/clients/BaseClient.java
@@ -24,6 +24,7 @@ public class BaseClient {
                 .requestFactory(HttpComponentsClientHttpRequestFactory::new)
                 .build();
     }
+
     private static ResponseEntity<String> prepareGatewayResponse(ResponseEntity<String> response) {
         if (response.getStatusCode().is2xxSuccessful()) {
             return response;
@@ -37,9 +38,11 @@ public class BaseClient {
 
         return responseBuilder.build();
     }
+
     protected ResponseEntity<String> get(String path, @Nullable Map<String, Object> parameters) {
         return makeAndSendRequest(HttpMethod.GET, path, parameters, null);
     }
+
     protected <T> ResponseEntity<String> post(String path, T body) {
         return post(path, null, body);
     }
@@ -47,6 +50,7 @@ public class BaseClient {
     protected <T> ResponseEntity<String> post(String path, @Nullable Map<String, Object> parameters, T body) {
         return makeAndSendRequest(HttpMethod.POST, path, parameters, body);
     }
+
     private <T> ResponseEntity<String> makeAndSendRequest(HttpMethod method, String path, @Nullable Map<String,
             Object> parameters, @Nullable T body) {
         HttpEntity<T> requestEntity = new HttpEntity<>(body, defaultHeaders());
@@ -63,6 +67,7 @@ public class BaseClient {
         }
         return prepareGatewayResponse(statsServerResponse);
     }
+
     private HttpHeaders defaultHeaders() {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
